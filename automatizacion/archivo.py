6d3dbe37cb3a6209f61774c4dda7b469fbbd6dc3
=manuel <=emdelapazt@gmail.com>
2025-06-14 19:56:21 -0500

    Añadir exportación de datos a CSV para el DataFrame de estudiantes

--- automatizacion/archivo.py
+++ automatizacion/archivo.py
@@ -5,5 +5,6 @@ estudiantes = pd.DataFrame({
     "nombre":["elmer","manuel","erika"],
     "edad":[list(np.arange(0,2)) for _ in range(3)]
 })
+estudiantes.to_csv("papu.csv",index=False)
 
 print(estudiantes)
